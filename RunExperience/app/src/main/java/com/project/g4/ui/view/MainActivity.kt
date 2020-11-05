1f696b242397bad4c8560b03967e9a3434f41fdf
Alexander Gabriel Luna Choquecota <lordalex27@gmail.com>
2020-11-04 19:49:25 -0500

    Lab06: Fragments and Bottom navigation bar implemented

--- RunExperience/app/src/main/java/com/project/g4/ui/view/MainActivity.kt
+++ RunExperience/app/src/main/java/com/project/g4/ui/view/MainActivity.kt
@@ -1,11 +1,48 @@
-package com.project.g4
+package com.project.g4.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.project.g4.R
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.navigationView)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            selectItem(it)
+        }
+//        mainNav.selectedItemId = R.id.nav_profile
+
+        //to avoid reload page
+        bottomNavigation.setOnNavigationItemReselectedListener { }
+    }
+
+    private fun selectItem(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> { setFragment(HomeFragment.newInstance()) }
+            R.id.nav_search -> { setFragment(SearchFragment.newInstance()) }
+            R.id.nav_running -> { setFragment(RunningFragment.newInstance()) }
+            R.id.nav_songs -> { setFragment(SongsFragment.newInstance()) }
+            R.id.nav_profile -> { setFragment(ProfileFragment.newInstance()) }
+        }
+        return true
+    }
+    fun setFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.mainFrame, fragment, fragment.javaClass.simpleName)
+            .addToBackStack(null)
+            .commit()
     }
 }
